faf38c03601e316bd4b433ff69e8894c8a473383
Muhammed Mustafa Geldi <swankydata9@gmail.com>
2025-03-03 23:49:41 +0300

    Initial Commit

---
+++ Weatherly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weatherly
+//
+//  Created by Muhammed Mustafa Geldi on 3.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatherlyTests/WeatherlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyTests.swift
+//  WeatherlyTests
+//
+//  Created by Muhammed Mustafa Geldi on 3.03.2025.
+//
+
+import Testing
+@testable import Weatherly
+
+struct WeatherlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
